35a5297d2f3ce5b5780fab22bfdfad0b712f3651
Aaron Huisinga <aaronhuisinga@gmail.com>
2014-04-24 00:44:08 -0500

    Add Renew and GetPricing methods
    Add a few more official Namecheap supported methods to the API.

---
+++ src/Namecheap/Command/Domains/Renew.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Namecheap\Command\Domains\Renew
+{
+    class Exception extends \Exception {}
+}
+
+namespace Namecheap\Command\Domains
+{
+    class Renew extends \Namecheap\Command\ACommand
+    {
+        public $domain = array();
+
+        public function command()
+        {
+            return 'namecheap.domains.renew';
+        }
+
+        public function params()
+        {
+            return array(
+                'DomainName'    => null,
+                'Years'            => 1,
+            );
+        }
+
+        /**
+         * Process domains array
+         */
+        protected function _postDispatch()
+        {
+            foreach ($this->_response->DomainRenewResult->attributes() as $key => $value)
+            {
+                $this->domain[$key] = (string) $value;
+            }
+        }
+
+        /**
+         * Get/set method for domain list, limited to 1024 characters
+         * @param string|array $value
+         * @return mixed
+         */
+        public function domainName($value = null)
+        {
+            if (null !== $value)
+            {
+                $this->setParam('DomainName', (string) substr($value, 0, 70));
+                return $this;
+            }
+            $this->getParam('DomainName');
+        }
+    }
+}
