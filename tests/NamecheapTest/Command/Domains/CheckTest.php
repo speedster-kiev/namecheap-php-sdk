7a52cfaa096b0bc52555231a293096f4a6771677
Maksym Karazieiev <mk@sunnyrentals.com>
2015-05-14 16:47:01 +0200

    Added first test

---
+++ tests/NamecheapTest/Command/Domains/CheckTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Maksym Karazieiev <mk@sunnyrentals.com>
+ */
+namespace NamecheapTest\Command\Domains {
+
+    include_once '/Users/maksym/www/namecheap-php-sdk/src/Namecheap/Api.php';
+    use Namecheap\Command\Domains\Check;
+
+    class CheckTest extends \PHPUnit_Framework_TestCase {
+
+        public function testCommandName() {
+            $command = new Check();
+            $this->assertEquals('namecheap.domains.check', $command->command());
+        }
+    }
+
+}
