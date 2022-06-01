2a87c341e792b4b3af4ab1d6e5ade5a9d7c167af
Aaron Huisinga <aaronhuisinga@gmail.com>
2022-06-01 08:27:58 -0500

    Code cleanup

--- src/Namecheap/Api.php
+++ src/Namecheap/Api.php
@@ -2,32 +2,8 @@
 
 namespace Namecheap;
 
-use Namecheap\Command\ACommand;
+use Namecheap\Command;
 use Namecheap\Exceptions\NamecheapException;
-
-include_once 'Config.php';
-include_once 'DnsRecord.php';
-include_once 'Command/ICommand.php';
-include_once 'Command/ACommand.php';
-include_once 'Command/Domains/GetList.php';
-include_once 'Command/Domains/GetContacts.php';
-include_once 'Command/Domains/GetInfo.php';
-include_once 'Command/Domains/Check.php';
-include_once 'Command/Domains/Create.php';
-include_once 'Command/Domains/GetTldList.php';
-include_once 'Command/Domains/Renew.php';
-include_once 'Command/Domains/Dns/SetDefault.php';
-include_once 'Command/Domains/Dns/SetCustom.php';
-include_once 'Command/Domains/Dns/GetList.php';
-include_once 'Command/Domains/Dns/GetHosts.php';
-include_once 'Command/Domains/Dns/SetHosts.php';
-include_once 'Command/Domains/Ns/Create.php';
-include_once 'Command/Domains/Ns/Delete.php';
-include_once 'Command/Domains/Ns/GetInfo.php';
-include_once 'Command/Domains/Ns/Update.php';
-include_once 'Command/Users/GetBalances.php';
-include_once 'Command/Users/GetPricing.php';
-include_once 'Command/Users/Address/GetList.php';
 
 class Api
 {
@@ -37,26 +13,26 @@ class Api
      * @var array
      */
     protected static $_commands = [
-        'domains.getList'        => 'Namecheap\Command\Domains\GetList',
-        'domains.getContacts'    => 'Namecheap\Command\Domains\GetContacts',
-        'domains.getInfo'        => 'Namecheap\Command\Domains\GetInfo',
-        'domains.create'         => 'Namecheap\Command\Domains\Create',
-        'domains.check'          => 'Namecheap\Command\Domains\Check',
-        'domains.renew'          => 'Namecheap\Command\Domains\Renew',
-        'domains.getTldList'     => 'Namecheap\Command\Domains\GetTldList',
-        'domains.dns.setDefault' => 'Namecheap\Command\Domains\Dns\SetDefault',
-        'domains.dns.setCustom'  => 'Namecheap\Command\Domains\Dns\SetCustom',
-        'domains.dns.getList'    => 'Namecheap\Command\Domains\Dns\GetList',
-        'domains.dns.getHosts'   => 'Namecheap\Command\Domains\Dns\GetHosts',
-        'domains.dns.setHosts'   => 'Namecheap\Command\Domains\Dns\SetHosts',
-        'domains.ns.create'      => 'Namecheap\Command\Domains\Ns\Create',
-        'domains.ns.delete'      => 'Namecheap\Command\Domains\Ns\Delete',
-        'domains.ns.getInfo'     => 'Namecheap\Command\Domains\Ns\GetInfo',
-        'domains.ns.update'      => 'Namecheap\Command\Domains\Ns\Update',
-        'users.getBalances'      => 'Namecheap\Command\Users\GetBalances',
-        'users.getPricing'       => 'Namecheap\Command\Users\GetPricing',
-        'users.address.getList'  => 'Namecheap\Command\Users\Address\GetList',
-        'users.address.getInfo'  => 'Namecheap\Command\Users\Address\GetInfo',
+        'domains.getList'        => Command\Domains\GetList::class,
+        'domains.getContacts'    => Command\Domains\GetContacts::class,
+        'domains.getInfo'        => Command\Domains\GetInfo::class,
+        'domains.create'         => Command\Domains\Create::class,
+        'domains.check'          => Command\Domains\Check::class,
+        'domains.renew'          => Command\Domains\Renew::class,
+        'domains.getTldList'     => Command\Domains\GetTldList::class,
+        'domains.dns.setDefault' => Command\Domains\Dns\SetDefault::class,
+        'domains.dns.setCustom'  => Command\Domains\Dns\SetCustom::class,
+        'domains.dns.getList'    => Command\Domains\Dns\GetList::class,
+        'domains.dns.getHosts'   => Command\Domains\Dns\GetHosts::class,
+        'domains.dns.setHosts'   => Command\Domains\Dns\SetHosts::class,
+        'domains.ns.create'      => Command\Domains\Ns\Create::class,
+        'domains.ns.delete'      => Command\Domains\Ns\Delete::class,
+        'domains.ns.getInfo'     => Command\Domains\Ns\GetInfo::class,
+        'domains.ns.update'      => Command\Domains\Ns\Update::class,
+        'users.getBalances'      => Command\Users\GetBalances::class,
+        'users.getPricing'       => Command\Users\GetPricing::class,
+        'users.address.getList'  => Command\Users\Address\GetList::class,
+        'users.address.getInfo'  => Command\Users\Address\GetInfo::class,
     ];
 
     /**
